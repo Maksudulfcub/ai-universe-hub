6a9dbbfdf9f7ef3cd4c88d52e4ed260215ee0d2d
Maksudul <maaksuduldish@gmail.com>
2025-03-02 22:55:28 +0600

    View details modal WIP

--- js/script.js
+++ js/script.js
@@ -7,9 +7,9 @@ const loadData = async () => {
 
 const displayItems = (items) => {
     const itemsContainer = document.getElementById('items-container');
+    itemsContainer.textContent = '';
 
     for (const item of items) {
-        console.log(item);
         const itemCard = document.createElement('div');
         itemCard.classList = `card bg-base-100 shadow-sm`;
         itemCard.innerHTML = `
@@ -28,12 +28,57 @@ const displayItems = (items) => {
                         <h2 class="card-title">${item.name}</h2>
                          <p class="text-gray-500 text-xs">${item.published_in}</p>
                      </div>
-                        <button class="btn btn-primary">Details</button>
+                        <button onClick="handleViewDetails('${item.id}');my_modal_5.showModal()" class="btn btn-primary">Details</button>
                     </div>
                 </div>
         `;
         itemsContainer.appendChild(itemCard);
     }
 }
+
+const handleViewDetails = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`)
+    const data = await res.json();
+    const dataById = data.data
+    viewDetails(dataById)
+}
+
+const viewDetails = (dataById) => {
+    console.log(dataById);
+    const modalContainer = document.getElementById('modal-container');
+    const viewDetailsModal = document.getElementById('view-details');
+    viewDetailsModal.innerHTML = `
+           <div class="space-y-5">
+                            <h3 class="font-semibold text-xl">Descriptions</h3>
+                            <div class="flex gap-5 justify-around">
+                                <h4 class="font-semibold text-green-500 p-5 bg-gray-200 rounded-lg">Basic</h4>
+                                <h4 class="font-semibold text-orange-400 p-5 bg-gray-200 rounded-lg">Pro</h4>
+                                <h4 class="font-semibold text-red-500 p-5 bg-gray-200 rounded-lg">Enterprise</h4>
+                            </div>
+                            <div class="flex gap-5 justify-around">
+                                <div>
+                                    <h2 class="card-title">Features</h2>
+                                    <p class="text-gray-400 text-xs">Features</p>
+                                    <p class="text-gray-400 text-xs">Features</p>
+                                    <p class="text-gray-400 text-xs">Features</p>
+                                </div>
+                                <div>
+                                    <h2 class="card-title">Features</h2>
+                                    <p class="text-gray-400 text-xs">Features</p>
+                                    <p class="text-gray-400 text-xs">Features</p>
+                                    <p class="text-gray-400 text-xs">Features</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <img src="" alt="">
+                            <h2 class="card-title mt-10">Features</h2>
+                            <p class="text-gray-400 text-xs">Features</p>
+                        </div>
+    `;
+    modalContainer.appendChild(viewDetailsModal);
+}
+
 
 loadData();
